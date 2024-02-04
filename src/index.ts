73600381cbc9ec43aa9c6e5f36d01bffe32b6d51
MasatakaItoh <ito@wd-flat.com>
2024-02-04 19:21:30 +0900

    feat: create Resolvers

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 import { typeDefs } from "./scheme";
+import db from "./_db";
+
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    reviews: () => db.reviews,
+    authors: () => db.authors,
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  // resolvers,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
